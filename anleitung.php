85120688429ec5b208dacfe04b3b5a4ce35e3a6f
torusblue129 <torusblue129@gmail.com>
2022-08-06 00:01:22 -0400

    Updated anleitung.php.

--- anleitung.php
+++ anleitung.php
@@ -25,90 +25,74 @@ AccessLogger::logRequest();
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<title><?php echo SERVER_NAME; ?></title>
-	<link rel="stylesheet" type="text/css" href="img/tutorial/main.css"/>
-	<link rel="stylesheet" type="text/css" href="img/tutorial/flaggs.css"/>
-	<meta name="content-language" content="en"/>
-	<meta http-equiv="imagetoolbar" content="no"/>
-	<script src="mt-core.js" type="text/javascript"></script>
-	<script src="new.js" type="text/javascript"></script>
-	<style type="text/css" media="screen"></style>
-</head>
+	<head>
+		<title><?php echo SERVER_NAME; ?></title>
+		<link rel="stylesheet" type="text/css" href="img/tutorial/main.css"/>
+		<link rel="stylesheet" type="text/css" href="img/tutorial/flaggs.css"/>
+		<meta name="content-language" content="en"/>
+		<meta http-equiv="imagetoolbar" content="no"/>
+		<script src="mt-core.js" type="text/javascript"></script>
+		<script src="new.js" type="text/javascript"></script>
+		<style type="text/css" media="screen"></style>
+	</head>
 
-<body class="webkit contentPage">
-<div class="wrapper">
-<div id="country_select"></div>
-<div id="header">
-	<h1>Welcome to <?php echo SERVER_NAME; ?></h1>
-</div>
+	<body class="webkit contentPage">
+		<div class="wrapper">
+		<div id="country_select"></div>
+		<div id="header">
+			<h1>Welcome to <?php echo SERVER_NAME; ?></h1>
+		</div>
 
-<div id="navigation">
-<a href="index.php" class="home"><img src="img/x.gif" alt="Travian"/></a>
-	<table class="menu">
-	<tr>
-		<td><a href="tutorial.php"><span>遊戲教學</span></a></td>
-		<td><a href="anleitung.php"><span>玩家指南</span></a></td>
-		<td><a href="http://forum.travian.com/" target="_blank"><span>遊戲論壇</span></a></td>
-		<td><a href="index.php?signup"><span>登記帳號</span></a></td>
-		<td><a href="index.php?login"><span>登入遊戲</span></a></td>
-	</tr>
-	</table>
-</div>
+		<div id="navigation">
+		<a href="index.php" class="home"><img src="img/x.gif" alt="Travian"/></a>
+			<table class="menu">
+			<tr>
+				<td><a href="tutorial.php"><span>遊戲教學</span></a></td>
+				<td><a href="anleitung.php"><span>玩家指南</span></a></td>
+				<td><a href="http://forum.travian.com/" target="_blank"><span>遊戲論壇</span></a></td>
+				<td><a href="index.php?signup"><span>登記帳號</span></a></td>
+				<td><a href="index.php?login"><span>登入遊戲</span></a></td>
+			</tr>
+			</table>
+		</div>
 
-<div id="content">
-<div class="grit">
+		<div id="content">
+		<div class="grit">
 
-	<h1>玩家指南</h1>
-		<p class="submenu">
-			<a href="anleitung.php">種族</a> |
-			<a href="anleitung.php?s=1">建築物</a> |
-			<a href="anleitung.php?s=3">常見問題</a> |
-			<a href="anleitung.php?s=4">文明點</a>
-		</p>
+		<h1>玩家指南</h1>
+			<p class="submenu">
+				<a href="anleitung.php">種族</a> |
+				<a href="anleitung.php?s=1">建築物</a> |
+				<a href="anleitung.php?s=3">常見問題</a> |
+				<a href="anleitung.php?s=4">文明點</a>
+			</p>
 
-<?php
-if(!isset($_GET['s'])) {
-$_GET['s'] = ""; }
-if ($_GET['s'] == "") {
-include("Templates/Anleitung/0.tpl"); }
-if ($_GET['s'] == "1") {
-include("Templates/Anleitung/1.tpl"); }
-if ($_GET['s'] == "3") {
-include("Templates/Anleitung/3.tpl"); }
-if ($_GET['s'] == "4") {
-include("Templates/Anleitung/4.tpl"); }
-?>
+		<?php
+		if(!isset($_GET['s'])) {
+		$_GET['s'] = ""; }
+		if ($_GET['s'] == "") {
+		include("Templates/Anleitung/0.tpl"); }
+		if ($_GET['s'] == "1") {
+		include("Templates/Anleitung/1.tpl"); }
+		if ($_GET['s'] == "3") {
+		include("Templates/Anleitung/3.tpl"); }
+		if ($_GET['s'] == "4") {
+		include("Templates/Anleitung/4.tpl"); }
+		?>
 
-<div class="footer"></div>
+		<div class="footer"></div>
 
-</div>
-<div id="iframe_layer" class="overlay">
-<div class="mask closer">
-</div>
+		<div id="iframe_layer" class="overlay"></div>
+		<div class="mask closer"></div>
 
-<div class="overlay_content">
+		<div class="overlay_content">
+			<a href="index.php" class="closer"><img class="dynamic_img" alt="Close" src="img/un/x.gif" /></a>
 
-<a href="index.php" class="closer"><img class="dynamic_img" alt="Close" src="img/un/x.gif" /></a>
-
-<h2>Anleitung</h2>
-
-
-
-<div id="frame_box" >
-
-</div>
-
-<div class="footer"></div>
-
-</div>
-
-
-
-</div>
-
-
-
-
-</body>
+		<h2>Anleitung</h2>
+		<div id="frame_box" >
+			</div>
+				<div class="footer"></div>
+			</div>
+		</div>
+	</body>
 </html>
