e72af56d365bdc0a97aff9e13258345cceba7657
torusblue129 <torusblue129@gmail.com>
2022-08-07 10:47:17 -0400

    Updated index.php.

--- index.php
+++ index.php
@@ -120,20 +120,21 @@ AccessLogger::logRequest();
 						<table>
 							<tbody>
 								<tr>
-									<th><?php echo $lang['index'][0][7]; ?>:</th>
-									<td><?php $return = mysqli_query($link, "SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE tribe IN(1, 2, 3)");
-                  echo ($users = !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0); ?></td>
+                  <td><?php
+											$return = mysqli_query($link, "SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE tribe IN(1, 2, 3)");
+											echo ($users = !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0);
+									?></td>
 								</tr>
 
 								<tr>
 									<th><?php echo $lang['index'][0][8]; ?>:</th>
-									<td><?php $return = mysqli_query($link,"SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (3600*24))." AND tribe IN(1, 2, 3)");
+									<td><?php $return = mysqli_query($link, "SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (3600*24))." AND tribe IN(1, 2, 3)");
                   echo !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0; ?></td>
 								</tr>
 
 								<tr>
 									<th><?php echo $lang['index'][0][9]; ?>:</th>
-									<td><?php $return = mysqli_query($link,"SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (60*10))." AND tribe IN(1, 2, 3)");
+									<td><?php $return = mysqli_query($link, "SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (60*10))." AND tribe IN(1, 2, 3)");
                   echo ($online = !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0); ?></td>
 								</tr>
 							</tbody>
