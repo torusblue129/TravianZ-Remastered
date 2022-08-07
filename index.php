85eccf43f155c0e0208dfe06ae0e5c7edff9f454
torusblue129 <torusblue129@gmail.com>
2022-08-07 10:51:27 -0400

    Updated index.php.

--- index.php
+++ index.php
@@ -134,9 +134,10 @@ AccessLogger::logRequest();
 								</tr>
 
 								<tr>
-									<th><?php echo $lang['index'][0][9]; ?>:</th>
-									<td><?php $return = mysqli_query($link, "SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (60*10))." AND tribe IN(1, 2, 3)");
-                  echo ($online = !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0); ?></td>
+                  <td><?php
+										   $return = mysqli_query($link,"SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (60*10))." AND tribe IN(1, 2, 3)");
+										   echo ($online = !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0);
+									?></td>
 								</tr>
 							</tbody>
 						</table>
