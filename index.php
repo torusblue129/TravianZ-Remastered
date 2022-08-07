dcf80673396ffae6b4ec49a7f232fdba23509b32
torusblue129 <torusblue129@gmail.com>
2022-08-07 10:45:25 -0400

    Updated index.php.

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ AccessLogger::logRequest();
 								<tr>
 									<th><?php echo $lang['index'][0][8]; ?>:</th>
 									<td><?php $return = mysqli_query($link,"SELECT Count(*) as Total FROM " . TB_PREFIX . "users WHERE timestamp > ".(time() - (3600*24))." AND tribe IN(1, 2, 3)");
-                  echo !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0); ?></td>
+                  echo !empty($return) ? mysqli_fetch_assoc($return)['Total'] : 0; ?></td>
 								</tr>
 
 								<tr>
