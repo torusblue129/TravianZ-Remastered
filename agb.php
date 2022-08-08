8716732d4c86992603032383e72134c1aab42699
torusblue129 <torusblue129@gmail.com>
2022-08-08 18:54:11 -0400

    Updated agb.php.

--- agb.php
+++ agb.php
@@ -22,6 +22,7 @@ include_once("GameEngine/Database.php");
 include_once("GameEngine/Lang/".LANG.".php");
 AccessLogger::logRequest();
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
